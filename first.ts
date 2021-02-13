4ac01098d2efeb322a578f4f7c0637338e337d84
Nayeem-uddin <nayeemu02@gmail.com>
2021-02-13 20:18:08 +0600

    value to declare name of the variable

--- first.ts
+++ first.ts
@@ -1,4 +1,16 @@
-let myName = 'nayeem';
-// myName = 50; //what is the problem here . then the problem arises that it is actully type sctrict
-myName = 'uddin';
-console.log(myName);
+// let myName = 'nayeem';
+// // myName = 50; //what is the problem here . then the problem arises that it is actully type sctrict
+// myName = 'uddin';
+// console.log(myName);
+
+
+
+//we have to declare the value type with the variable
+let surName:string = 'nayeem';
+let age:number = 20;
+let isAble:boolean = true; //it will give us the benefit whenever we want to console.log this variable the another benefit is that whenver anyone wants to use my code he/she can easily understand whether it is variable or function. and if it is variable then which type is this variable
+
+
+const department:string = 'cse';
+department = 'eee' // we can't change as we know that if we declare anything as const then we can't change the value.
+
